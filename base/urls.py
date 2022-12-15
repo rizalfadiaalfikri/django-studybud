fb86e24eaeaeca54916057e1c8c718eb6e4a9127
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-15 07:39:23 +0700

    user login

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -12,12 +13,26 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
